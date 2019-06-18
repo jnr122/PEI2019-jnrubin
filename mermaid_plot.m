6165de2871155db990cb5668caa1022888b3d506
jonah <jonah.n.rubin@gmail.com>
2019-06-18 12:15:28 -0400

    last 30 plotted

--- mermaid_plot.m
+++ mermaid_plot.m
@@ -9,7 +9,31 @@ function [data] = mermaid_plot(float_name)
 %
 % Last modified by Jonah Rubin, 6/17/19
 
-raw_data = webread('http://geoweb.princeton.edu/people/simons/SOM/P017_030.txt');
-data = strsplit(raw_data, '\n')
+raw_data = webread(strcat('http://geoweb.princeton.edu/people/simons/SOM/', float_name, '_030.txt'));
+data = strsplit(raw_data, '\n');
 
-x = data(1)
+data_points = [];
+
+figure(1)
+title(strcat(float_name, ': last 30 locations'))
+xlabel('Latitude')
+ylabel('Longitude')
+
+hold on
+
+for i = 1:length(data)-1
+   entry = data(i);
+   split_entry = strsplit(entry{1});
+   
+   float.name = cell2mat(split_entry(1));
+   float.lat  = str2double(split_entry(4));
+  
+   float.long  = str2double(split_entry(5)); 
+   float.loc  = geopoint(str2double(split_entry(4)), str2double(split_entry(5)));
+   float.date = split_entry(2);
+   float.time = split_entry(3); 
+ 
+   data_points = [data_points, float];
+   geoshow(float.loc)
+end
+
