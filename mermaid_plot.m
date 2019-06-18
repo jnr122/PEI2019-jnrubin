cf34add4c8b4554fba6bd48544fb22e62938becf
jonah <jonah.n.rubin@gmail.com>
2019-06-18 13:06:12 -0400

    legend and age fading

--- mermaid_plot.m
+++ mermaid_plot.m
@@ -9,18 +9,19 @@ function [data] = mermaid_plot(float_name)
 %
 % Last modified by Jonah Rubin, 6/17/19
 
+% pull data
 raw_data = webread(strcat('http://geoweb.princeton.edu/people/simons/SOM/', float_name, '_030.txt'));
 data = strsplit(raw_data, '\n');
-
 data_points = [];
 
+% set up map
 figure(1)
 title(strcat(float_name, ': last 30 locations'))
 xlabel('Latitude')
 ylabel('Longitude')
-
 hold on
 
+% make float structs, plot
 for i = 1:length(data)-1
    entry = data(i);
    split_entry = strsplit(entry{1});
@@ -31,9 +32,18 @@ for i = 1:length(data)-1
    float.long  = str2double(split_entry(5)); 
    float.loc  = geopoint(str2double(split_entry(4)), str2double(split_entry(5)));
    float.date = split_entry(2);
-   float.time = split_entry(3); 
- 
+   float.time = split_entry(3);  
+
    data_points = [data_points, float];
-   geoshow(float.loc)
+   plot_map = plot3(float.lat,float.long, 2600, 'color', [i/length(data) i/length(data) i/length(data)],'marker','.','markersize',15 ) 
+   %geoshow(float.loc)
+   
 end
 
+% make legend
+plot_map(1) = plot(NaN,NaN,'sk', 'markersize', 6);
+plot_map(2) = plot(NaN,NaN,'.k', 'markersize', 15);
+plot_map(3) = plot(NaN,NaN,'.r', 'markersize', 15);
+legend(plot_map, 'Oldest','Latest','Prediction');
+
+     
